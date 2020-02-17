61a925e00c56edfac81358e660d9e1adfd30a80e
Christian Perez <perezchristian1012@gmail.com>
2020-02-17 17:22:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  newTask()
+const createTaskForm = document.getElementById("create-task-form");
+const deleteTasks = document.getElementById("delete-task-form");
+
+createTaskForm.addEventListener("submit", createNewTask);
+deleteTasks.addEventListener("submit", deleteAllTasks);
 });
 
-function newTask() {
-  document.getElementById("create-task-form").addEventListener("submit", function(event) {
-  event.preventDefault();
-    let task = document.querySelector("#new-task-description");
-    let li = document.createElement('li');
-    li.textContent = task.value;
-    document.getElementById('tasks').appendChild(li);
-  });
-  document.getElementById("create-task-form").reset();
-}
+const createNewTask = event => {
+event.preventDefault();
+const newTaskDescription = document.getElementById("new-task-description");
+const newTask = document.createElement("li");
+newTask.innerText = newTaskDescription.value;
+console.log(newTask);
+
+appendNewTask(newTask);
+event.target.reset();
+};
+
+const appendNewTask = task => {
+document.getElementById("tasks").appendChild(task);
+};
+
+const deleteAllTasks = event => {
+event.preventDefault();
+const findTasks = document.getElementById("tasks");
+console.log(findTasks);
+findTasks.innerText = "";
+};
